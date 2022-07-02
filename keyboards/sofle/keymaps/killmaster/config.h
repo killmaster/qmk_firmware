e0410507cb259d27ff54df36a86c322957003574
Carlos Martins <carlosmartins8@gmail.com>
2022-07-02 21:47:26 +0100

    some changes and a new layout for sofle w/o LEDs

--- keyboards/sofle/keymaps/killmaster/config.h
+++ keyboards/sofle/keymaps/killmaster/config.h
@@ -29,45 +29,6 @@ for more options.
    #define OLED_FADE_OUT_INTERVAL 10
    #define OLED_BRIGHTNESS 100
 #endif
-#ifdef RGB_MATRIX_ENABLE
-
-#define RGBLED_NUM 72
-#define DRIVER_LED_TOTAL RGBLED_NUM
-#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 120 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
-#define RGB_MATRIX_HUE_STEP 8
-#define RGB_MATRIX_SAT_STEP 8
-#define RGB_MATRIX_VAL_STEP 8
-#define RGB_MATRIX_SPD_STEP 10
-#define RGB_MATRIX_KEYPRESSES
-#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-#define RGB_MATRIX_SPLIT {36,36}
-#define SPLIT_TRANSPORT_MIRROR
-#define ENABLE_RGB_MATRIX_CYCLE_ALL
-#define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
-#define ENABLE_RGB_MATRIX_MULTISPLASH
-#define ENABLE_RGB_MATRIX_DIGITAL_RAIN
-#endif
-
-#ifdef RGBLIGHT_ENABLE
-#define RGBLIGHT_SPLIT
-#define RGBLED_NUM 70
-#define RGB_SPLIT {36,36}
-#define RGBLIGHT_LIMIT_VAL 120
-//   #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-// /*== all animations enable ==*/
-   #define RGBLIGHT_ANIMATIONS
-// /*== or choose animations ==*/
-//   #define RGBLIGHT_EFFECT_BREATHING
-   //#define RGBLIGHT_EFFECT_RAINBOW_MOOD
-   //#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-//   #define RGBLIGHT_EFFECT_SNAKE
-//   #define RGBLIGHT_EFFECT_KNIGHT
-//   #define RGBLIGHT_EFFECT_CHRISTMAS
-//   #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-//   #define RGBLIGHT_EFFECT_RGB_TEST
-//   #define RGBLIGHT_EFFECT_ALTERNATING
-#endif
-
 #define MEDIA_KEY_DELAY 2
 
 #define USB_POLLING_INTERVAL_MS 1
